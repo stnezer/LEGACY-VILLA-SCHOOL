da7fd7e9e2e75b2eb6bdcaf5a12a79f928d25ce4
Stnezer <Stnezer@icloud.com>
2024-05-17 15:00:37 -0800

    second test push

--- portrait.js
+++ portrait.js
@@ -1,10 +1,25 @@
+// const root = document.getElementById('root');
+
+// window.addEventListener('orientationchange', function(
+// ) {
+//   if (window.innerWidth < 768 && window.innerHeight < window.innerWidth) {
+//     root.style.transform = 'rotate(-90deg)';
+//   } else {
+//     root.style.transform = 'rotate(0)';
+//   }
+// });
+
 const root = document.getElementById('root');
 
 window.addEventListener('orientationchange', function(
 ) {
   if (window.innerHeight < window.innerWidth) {
-    root.style.transform = 'rotate(-90deg)';
+    // Disable the rotation effect on landscape mode
+    root.style.transform = 'none';
+    root.style.width = '100vw';
+    root.style.height = '100vh';
   } else {
-    root.style.transform = 'rotate(0)';
+    root.style.width = '100vh';
+    root.style.height = '100vw';
   }
 });
